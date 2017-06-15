bbd15140331d795dd3c77286bc7270934c8c315b
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2017-06-15 12:36:18 -0500

    Feature - Adecuaciones cliente
    -Se realizaron algunos ajustes a la clase Client para enviar archivos al servidor y cerrar el programa cuando el servidor cierre conexiones.
    -Se agregaron nuevos archivos de ejemplos para enviar al servidor.

--- src/client/Client.java
+++ src/client/Client.java
@@ -13,11 +13,9 @@ import static client.UtilClient.validateFile;
  */
 
 public class Client {
-    private String ip, port;
-    private Socket socket;
     private static final String PATH = "src/inputFiles/";
-
-    Client() {}
+    private Socket socket;
+    private String ip, port;
 
     public void start() throws IOException {
         getIPandPort();
@@ -26,24 +24,27 @@ public class Client {
     }
 
     private void getIPandPort(){
-        System.out.println("Welcome to General Store!");
+        String msgIp = "Please write the IP number to connect to server:";
+        String msgPort = "Please write the port number the server:";
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             do {
-                System.out.println("Please write the IP number to connect to server:");
+                System.out.println(msgIp);
                 ip = reader.readLine();
+                msgIp = "Please write a valid IP number:";
             } while (!validIPAddress(ip));
-            System.out.println("IP: "+ip);
 
             do {
-                System.out.println("Please write the port number the server:");
+                System.out.println(msgPort);
                 port = reader.readLine();
+                msgPort = "Please write a valid port number";
             } while (!validPortNumber(port));
-            System.out.println("Port: "+port);
         } catch (IOException e){
+            System.err.println("Error read line IP or port number");
             e.printStackTrace();
+            System.exit(1);
         }
     }
 
@@ -52,6 +53,7 @@ public class Client {
             socket = new Socket(ip, Integer.parseInt(port));
         } catch (UnknownHostException e){
             System.err.println("Don't know about host " + ip);
+            e.printStackTrace();
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " + ip + ":" + port + ". Check that the server is running.");
@@ -61,43 +63,56 @@ public class Client {
     }
 
     private void communication() throws IOException {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        BufferedReader userIn = null;
+        PrintWriter out;
+        BufferedReader in;
+        BufferedReader userIn;
+
+        System.out.println("Welcome to General Store!");
 
         try {
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             userIn = new BufferedReader(new InputStreamReader(System.in));
 
+            System.out.println(in.readLine());
+
+            Listen listen = new Listen(this, in);
+            listen.start();
+
             String userInput;
-            do {
-                userInput = userIn.readLine();
+            while ((userInput = userIn.readLine()) != null && !userInput.equals("CLOSE")){
                 String inputs[] = userInput.split("\\s+");
-                if (inputs.length > 1 && inputs[0].equals("send")){
-                    if (validateFile(PATH + inputs[1])){
-                        sendFile(inputs[1], out, in);
-                    }else{
-                        System.out.println("File doesn't exist");
-                    }
+                if (inputs.length > 1 && inputs[0].equals("SEND")){
+                    send(inputs[1], out);
                 }
-            } while (!userInput.equals("close"));
+            }
         } catch (IOException e) {
+            System.err.println("Error read input");
             e.printStackTrace();
+            System.exit(1);
         } finally {
-            if (out != null){
-                out.close();
-            }
-            if (in != null){
-                in.close();
-            }
-            if (userIn != null){
-                userIn.close();
-            }
+            exit();
         }
     }
 
-    private void sendFile(String file, PrintWriter out, BufferedReader in) throws IOException {
+    public void exit() throws IOException{
+        socket.close();
+        System.exit(0);
+    }
+
+    private void send(String file, PrintWriter out){
+        if (validateFile(PATH + file)){
+            try {
+                sendFile(file, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("File doesn't exist");
+        }
+    }
+
+    private void sendFile(String file, PrintWriter out) throws IOException {
         BufferedReader inFile = null;
         out.println("NEWFILE");
         out.println(file);
@@ -108,13 +123,37 @@ public class Client {
                 out.println(inputFile);
             }
         } catch (FileNotFoundException e) {
-            System.err.println("I/O Exception");
+            System.err.println("I/O Exception: File not found");
             e.printStackTrace();
-            System.exit(1);
         } finally {
             out.println("ENDFILE");
             if(inFile != null){
                 inFile.close();
+            }
+            System.out.println("File " + file + " send");
+        }
+    }
+
+    public class Listen extends Thread{
+        Client client;
+        BufferedReader in;
+
+        Listen(Client client, BufferedReader in) {
+            this.client = client;
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String line;
+                while ((line = in.readLine()) != null){
+                    if (line.equals("CLOSE")){
+                        client.exit();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -1,12 +1,10 @@
 package client;
 
-
 /**
  * Created by Roberto on 30/05/17.
  */
 
 public class MainClient {
-
     public static void main(String[] args) throws Exception{
         Client client = new Client();
         client.start();
